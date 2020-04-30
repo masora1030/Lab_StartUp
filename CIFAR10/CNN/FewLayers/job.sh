3c8bd81f1f918552952b9859326d22e747f90b0f
masora <soraemonpockt@icloud.com>
2020-05-01 03:37:40 +0900

    add result of CIFAR10 CNN FewLayers

---
+++ CIFAR10/CNN/FewLayers/job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p pascal
+#SBATCH --nodes 1
+#SBATCH -J job_name
+
+. /etc/profile.d/modules.sh
+module load cuda
+module load openmpi
+
+python CIFAR10_CNN_by_Pytorch.py
+# mpiexec -np 4 --npernode 1 ./a.out
