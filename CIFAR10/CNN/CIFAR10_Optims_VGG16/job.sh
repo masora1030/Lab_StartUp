2f481c4fe485c6d532a5cc5671c9cb441cc39181
masora <soraemonpockt@icloud.com>
2020-05-11 19:53:28 +0900

    add Comparison Optimizer with VGG16

---
+++ CIFAR10/CNN/CIFAR10_Optims_VGG16/job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p pascal
+#SBATCH --nodes 1
+#SBATCH -J job_name
+
+. /etc/profile.d/modules.sh
+module load cuda
+module load openmpi
+
+python OptimsComparison_in_CIFAR10_VGG16.py
+# mpiexec -np 4 --npernode 1 ./a.out
